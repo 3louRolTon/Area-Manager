ef5612b2b4efb206764f1a8479986935ac3b8a65
3louRoolTon <skrobat.yura@gmail.com>
2022-01-17 14:27:32 +0300

    fix register user

--- resources/js/components/Admin/Register.vue
+++ resources/js/components/Admin/Register.vue
@@ -1,7 +1,10 @@
 <template>
     <div>
         <form @submit.prevent="submit">
-            <div class="alert alert-success" v-show="success">Ticket created successfully.</div>
+            <div class="alert alert-success" v-show="success">Пользователь создан успешно.</div>
+            <div class="alert alert-danger" v-if="errors && errors.message">
+                {{ errors.message }}
+            </div>
             <div class="form-group row">
                 <label for="role" class="col-md-4 col-form-label text-md-right">Роль пользователя</label>
 
@@ -11,7 +14,7 @@
                                   placeholder="Выберите роль">
                     </model-select>
                     <div class="alert alert-danger" v-if="errors && errors.role">
-                        {{ errors.role[0] }}
+                        {{ errors.role }}
                     </div>
                 </div>
             </div>
@@ -22,10 +25,11 @@
                 <div class="col-md-6">
                     <model-select id="area" name="area" :options="area_options"
                                   v-model="area_item"
-                                  placeholder="select area">
+                                  placeholder="select area"
+                                  @input="loadDistrict">
                     </model-select>
                     <div class="alert alert-danger" v-if="errors && errors.area">
-                        {{ errors.area[0] }}
+                        {{ errors.area }}
                     </div>
                 </div>
             </div>
@@ -39,7 +43,7 @@
                                   placeholder="select district">
                     </model-select>
                     <div class="alert alert-danger" v-if="errors && errors.district">
-                        {{ errors.district[0] }}
+                        {{ errors.district }}
                     </div>
                 </div>
             </div>
@@ -81,10 +85,15 @@
     import { ModelSelect } from 'vue-search-select'
 
     export default {
+        props: [
+            'roles',
+            'areas',
+            'token'
+        ],
         data() {
             return {
                 role_options: [
-                    {value: 0, text: ' '}
+                    {value: 0, text: ''}
                 ],
                 role_item: {
                     value: '0',
@@ -123,19 +132,66 @@
             }
         },
         mounted() {
+            var self = this
 
+            JSON.parse(this.areas).forEach(function (value) {
+                self.area_options.push({value: value.id, text: value.area_name})
+            })
+
+            JSON.parse(this.roles).forEach(function (value) {
+                self.role_options.push({value: value, text: value})
+            })
         },
         methods: {
             submit() {
+                this.errors = {};
+                this.success = false;
+
+                if(this.role_item.value !== "admin" && this.role_item.value !== "user" && this.role_item.value !== "validate"){
+                    this.errors.role = "Не выбрана роль";
+                    return
+                }
+                if(this.area_item.value == 0 || this.area_item.value == "") {
+                    this.errors.area = "Не выбрана область";
+                    return
+                }
+                if(this.role_item.value !== "admin"){
+                    this.errors.district = "Не выбран район";
+                    return
+                }
+                this.fields.role = this.role_item.value
+                this.fields.area = this.area_item.value
+                this.fields.district = this.district_item.value
+
                 axios.post('/api/user', this.fields).then(response => {
                     this.fields = {};
                     this.success = true;
-                    this.errors = {};
+                }).catch(error => {
+                    console.log(error.response.data);
+                    console.log('Error');
+                });
+            },
+            loadDistrict() {
+                if(this.area_item.value === this.area_value_last.value) return;
+
+                this.area_value_last.value = this.area_item.value;
+
+                if(this.area_item.value == "" || this.area_item.value == 0){
+                    this.district_options = [{value: 0, text: ''}];
+                }
+
+                var self = this
+                this.district_options = [];
+                axios.get('/api/districts/area_id/'+this.area_item.value).then(response => {
+
+                    $.each(response.data.data, function (i, data) {
+                        self.district_options.push({value: data.id, text: data.name});
+                    });
+
                 }).catch(error => {
                     if (error.response.status == 422) {
-                        this.errors = error.response.data.errors;
+                        this.errors = { area: error.response.data.data.area_id};
                     }
-                    console.log('Error');
                 });
             }
         },
