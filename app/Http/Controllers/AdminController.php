959283473d721b20c7a2d5c211645a0e8c46d88f
3louRoolTon <skrobat.yura@gmail.com>
2022-02-05 00:09:33 +0300

    add json download button

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,10 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Models\Area;
 use App\Models\District;
+use App\Models\City;
+use App\Models\Street;
+use App\Models\House;
 use App\Models\User;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 
 class AdminController extends Controller
 {
@@ -74,5 +77,65 @@ class AdminController extends Controller
 
         //return $data;
         return view('admin.update', $data);
+    }
+
+    public function DownloadJson()
+    {
+        $user = Auth::user();
+        $json = [];
+        if($user->hasRole('super-admin')) {
+            $areas = [];
+            foreach (Area::all() as $area){
+                $districts = [];
+                foreach (District::where('area_id', $area->id)->get() as $district){
+                    $cities = [];
+                    foreach (City::where('district_id', $district->id)->get() as $city) {
+                        $streets = [];
+                        foreach (Street::where('city_id', $city->id)->get() as $street){
+                            $houses = House::where('street_id', $street->id)->whereNotNull('spot_id')->select('house_number', 'information', 'spot_id')->get();
+                            if(count($houses) > 0){
+                                $streets[] = [
+                                    'name' => $street->street_name,
+                                    'type' => $street->street_type,
+                                    'houses' => $houses
+                                ];
+                            }
+                        }
+                        if(count($streets) > 0){
+                            $cities[] = [
+                                'name' => $city->city_name,
+                                'type' => $city->city_type,
+                                'category' => $city->city_category,
+                                'spot_id' => $city->spot_id,
+                                'streets' => $streets
+                            ];
+                        } else if($city->spot_id) {
+                            $cities[] = [
+                                'name' => $city->city_name,
+                                'type' => $city->city_type,
+                                'category' => $city->city_category,
+                                'spot_id' => $city->spot_id,
+                                'streets' => null
+                            ];
+                        }
+                    }
+                    if(count($cities) > 0){
+                        $districts[] = [
+                            'name' => $district->district_name,
+                            'cities' => $cities
+                        ];
+                    }
+                }
+                if(count($districts) > 0){
+                    $areas[] = [
+                        'name' => $area->area_name,
+                        'districts' => $districts
+                    ];
+                }
+            }
+            $jsonFile = time() . '_file.json';
+            Storage::put('upload/json/'.$jsonFile, json_encode($areas, JSON_UNESCAPED_UNICODE));
+            return Storage::download('upload/json/'.$jsonFile);
+        }
     }
 }
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -5,9 +5,18 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">Добавить пользователя</div>
+                    <div class="card-header"></div>
 
                     <div class="card-body">
+                        <form method="GET" action="/admin/download">
+                            <div class="form-group row mb-0">
+                                <div class="col-md-8 offset-md-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        Загрузить json
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
